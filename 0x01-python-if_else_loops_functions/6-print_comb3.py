ec9548c6529377c8fa65702d471bcbf6b7f170a2
ralvztech <info@ralvz.com>
2023-11-01 05:53:16 +0100

    if else loop scripting

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(i, 10):
+        if j != 0:
+            if i == 8 and j == 9:
+                print("{}{}".format(i,j))
+            else:
+                print("{}{}".format(i,j), end = ',  ')
