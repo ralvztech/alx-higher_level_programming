bc2aab5c3131a42c1e18ccb045c26db09c6531c8
ralvztech <info@ralvz.com>
2023-11-01 11:12:47 +0100

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
-    for j in range(i, 10):
-        if j != 0:
+    for j in range(i+1, 10):
             if i == 8 and j == 9:
                 print("{}{}".format(i,j))
             else:
