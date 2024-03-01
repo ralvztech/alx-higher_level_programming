63d7e9e39af2a14b0b8bdbd1c1deae31176b37ce
ralvztech <info@ralvz.com>
2024-03-01 13:10:53 -0800

    Python network scripting

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
 """
-Displays the value of the X-Request-Id variable
-found in the header of the response
-"""
-import requests
-from sys import argv
+Sends a request to the URL and displays the value of the variable X-Request-Id
+in the response header"""
 
-if __name__ == "__main__":
-    main(argv)
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-def main(argv):
-    """
-    Method that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable
-    found in the header of the response
-    """
-    url = argv[1]
-    r = requests.get(url)
-    print(r.headers.get('X-Request-Id'))
+    res = get(argv[1])
+    print(res.headers.get('X-Request-Id'))
