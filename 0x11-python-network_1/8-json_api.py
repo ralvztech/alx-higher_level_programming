795f59b08f5aea003f94d56e06cd776a3b809fcf
ralvztech <info@ralvz.com>
2024-03-01 13:22:43 -0800

    Python network scripting

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""
-Module  sends a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
-"""
-import requests
-from sys import argv
+"""Sends a request to the URL and displays the body of the response."""
 
 
-def main(argv):
-    """
-    Script that takes in a letter and sends a POST request to
-    http://0.0.0.0:5000/search_user with the letter as a parameter.
-    """
-    if len(argv) < 2:
-        q = ""
-    else:
-        q = argv[1]
-    payload = {'q': q}
-    url = "http://0.0.0.0:5000/search_user"
-    r = requests.post(url, data=payload)
-    try:
-        result = r.json()
-        if bool(result) is False:
-            print("No result")
+if __name__ == '__main__':
+    from requests import post
+    from sys import argv
+
+    URL = 'http://0.0.0.0:5000/search_user'
+    data = {'q': argv[1] if len(argv) >= 2 else ""}
+    response = post(URL, data)
+
+    type_res = response.headers['content-type']
+
+    if type_res == 'application/json':
+        result = response.json()
+        _id = result.get('id')
+        name = result.get('name')
+        if (result != {} and _id and name):
+            print("[{}] {}".format(_id, name))
         else:
-            print("[{}] {}".format(result['id'], result['name']))
-    except:
-        print("Not a valid JSON")
-
-if __name__ == "__main__":
-    main(argv)
+            print('No result')
+    else:
+        print('Not a valid JSON')
